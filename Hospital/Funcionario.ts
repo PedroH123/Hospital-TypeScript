e9b063fd096559b7f9cc3a487c31506cae2245e2
PedroH123 <pedro_3ano@hotmail.com>
2019-11-26 17:26:46 +0000

    Funcionario.ts

---
+++ Hospital/Funcionario.ts
@@ -0,0 +1,17 @@
+namespace Hospital{
+
+    export class Funcionario extends Pessoa{
+
+        private _codFuncionario:number;
+    
+
+             public getCodFuncionario(){
+            return this._codFuncionario;
+        }
+
+        public setCodFuncionario(codigo:number){
+            this._codFuncionario = codigo;
+        }
+
+     }
+}
